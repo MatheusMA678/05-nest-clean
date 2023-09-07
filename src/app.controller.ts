31fc8ad7553ef439556f9877953f2538c1298cc0
MatheusMA678 <matheusma2510@hotmail.com>
2023-09-06 23:23:27 -0400

    Aula 3 - Configurando ESLint e Prettier

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common'
+import { AppService } from './app.service'
 
-@Controller("/api/v1")
+@Controller('/api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
-  @Get("/hello")
+  @Get('/hello')
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 
-  @Post("/products")
+  @Post('/products')
   store(): string {
-    return "Produto criado";
+    return 'Produto criado'
   }
 }
