70b8742a49952b602a1a7c503aa64ff856b1d6f6
MatheusMA678 <matheusma2510@hotmail.com>
2023-09-06 22:49:40 -0400

    Aula 2 - Módulos, Serviços e Controllers

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller("/api/v1")
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get("/hello")
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post("/products")
+  store(): string {
+    return "Produto criado";
+  }
+}
